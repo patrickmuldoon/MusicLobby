6842bd743aea4706748db27e2aeabbe34c417bf4
Patrick <patrickmuldoon20@yahoo.com>
2019-02-24 22:51:32 -0600

    Added Search controller which will be used for querying spotify Api as well as Artists controllers which will be used to search by Artist. Still need to do authentication for spotify calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get '/artists/:id', to:'artists#show', as: 'artist'
+
+  get '/artists', to: 'artists#index'
+
   devise_for :users
   root 'static_pages#home'
   get    '/home',    to: 'static_pages#home'
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/login',   to: 'static_pages#home'
-
+  get 'search/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
